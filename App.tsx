a10296ac185917556d00a89f350b24170ba3a6af
Kaushik119 <32000979+Kaushik119@users.noreply.github.com>
2023-04-16 22:28:51 -0700

    NavBar complete

--- App.tsx
+++ App.tsx
@@ -24,74 +24,46 @@ const Stack = createNativeStackNavigator();
 function ShowTabs(){
   return (
   <Tab.Navigator 
-  // screenOptions={({ route }) => ({
-  //   tabBarIcon: ({ focused, color, size }) => {
-  //     let iconName;
-
-  //     // if (route.name === 'Challenges') {
-  //     //   iconName = focused
-  //     //     ? 'ios-information-circle'
-  //     //     : 'ios-information-circle-outline';
-  //     // } else if (route.name === 'Settings') {
-  //     //   iconName = focused ? 'ios-list' : 'ios-list-outline';
-  //     // }
-
-  //     // // You can return any component that you like here!
-  //     // return <Icon name={iconName} size={size} color={color} />;
-  //     return <Image source={{ uri: 'https://i.imgur.com/qajrJEV.png'}}
-  //   />
-  //   },
-  //   tabBarActiveTintColor: 'tomato',
-  //   tabBarInactiveTintColor: 'gray',
-  // })}
-  
     screenOptions={({route}) => ({
       tabBarIcon:({focused, color, size}) => {
         let iconName;
-        let iconColor;
-        let iconGroup;
-        
-        iconColor = focused 
-        ? "#9B9595"
-        : "#F9A800"
         
         if (route.name === 'Challenges') {
-          iconName = "target-2"
-          iconGroup = "miscellaneous"
-          console.log({iconName})
-          // return <Icon name = "target-2" group = "miscellaneous" />
+          iconName = focused
+          ? "https://imgur.com/FTvnYD3.png"
+          : "https://imgur.com/yo7tEEr.png"
         } else if (route.name === 'Leagues') {
-          iconName = "star"
-          iconGroup = "new-year"
-          console.log({iconName})
-          // return <Icon name = "target-2" group = "miscellaneous" />
+          iconName = focused
+          ? "https://imgur.com/fMqLrA2.png"
+          : "https://imgur.com/PZgaF7K.png"
         } else if (route.name === 'Add') {
-          iconName = "plus"
-          iconGroup = "ui-interface"
-          console.log({iconName})
-          // return <Icon name = "target-2" group = "miscellaneous" />
+          iconName = focused
+          ? "https://imgur.com/0MHiWup.png"
+          : "https://imgur.com/iWwOCAG.png"
         } else if (route.name === 'Search') {
-          iconName = "search"
-          iconGroup = "essential"
-          console.log({iconName})
-          // return <Icon name = "target-2" group = "miscellaneous" />
+          iconName = focused
+          ? "https://imgur.com/yUUEUim.png"
+          : "https://imgur.com/NUVX70E.png"
         } else if (route.name === 'Profile') {
-          iconName = "profile"
-          iconGroup = "basic"
-          console.log({iconName})
-          // return <Icon name = "target-2" group = "miscellaneous" />
+          iconName = focused
+          ? "https://imgur.com/zZnCC7M.png"
+          : "https://imgur.com/R5bFbb3.png"
         }
-        // return <Icon name = "target-2" group = "miscellaneous" />
 
-        // return <Icon name = {iconName} group = {iconGroup} />
-        return <Image style ={{width : 30, height : 30}} source={{uri: 'https://i.imgur.com/qajrJEV.png'}}/>
-      }
+       return <Image style ={{width : 30, height : 30}} source={{uri: iconName}}/>
+      },
+      tabBarActiveTintColor: '#F9A800',
+      tabBarInactiveTintColor: '#9B9595',
+      tabBarStyle : { 
+        height :90 , 
+        borderRadius : 40, 
+        overflow : 'hidden'},
     })
   }
   > 
     <Tab.Screen name="Challenges" component={Challenge} options={{ headerShown: false }}/>
     <Tab.Screen name="Leagues" component={LeaguesPage} options={{ headerShown: false }}/>
-    <Tab.Screen name="Add" component={AddPage} options={{ headerShown: false }}/>
+    <Tab.Screen name="Add" component={AddPage} options={{headerShown: false}}/>
     <Tab.Screen name="Search" component={SearchPage} options={{ headerShown: false }}/>
     <Tab.Screen name="Profile" component={ProfilePage} options={{ headerShown: false }}/>
   </Tab.Navigator>
