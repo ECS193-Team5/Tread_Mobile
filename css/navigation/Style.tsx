19b155908e55886ae1c73bd6c435aa52b4561958
pdkainth <33015564+pdkainth@users.noreply.github.com>
2023-04-18 20:38:09 -0700

    android specific css

--- css/navigation/Style.tsx
+++ css/navigation/Style.tsx
@@ -2,14 +2,16 @@ import { Platform, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 	navBarStyle: {
-		height :85 ,
+
+		height : (Platform.OS === 'android') ? 70 : 85,
 		overflow : 'hidden',
-		borderRadius : 40,
+		borderTopLeftRadius: 40,
+		borderTopRightRadius: 40,
 		position: 'absolute',
 		borderColor : "#014421",
 		borderWidth : 0.5,
 		borderTopColor : "#014421",
-		borderTopWidth :0.5
+		borderTopWidth : (Platform.OS === 'android') ? 0.75 : 0.5,
 	}
 });
 
