ba2e823e279ddf26e25bd2e815ec4a57a2b886f2
pdkainth <33015564+pdkainth@users.noreply.github.com>
2023-05-09 21:18:17 -0700

    finished getting profile photo

--- css/profile/Style.tsx
+++ css/profile/Style.tsx
@@ -5,6 +5,67 @@ const ProfileStyles = StyleSheet.create({
         flexDirection: 'column',
         flex: 1,
         backgroundColor : "#E0E7E3"
+    },
+
+    TopSeparator: {
+        flex: 3
+    },
+
+    ProfileContainer: {
+        flex: 23,
+        flexDirection: 'column',
+        // backgroundColor: 'blue',
+        alignItems: 'center'
+    },
+
+    ProfileCard: {
+        flex : 1,
+        width : "90%",
+        borderRadius : 20,
+        flexDirection : 'column',
+        alignItems : 'center',
+        backgroundColor : "#FFFFFF",
+    },
+
+    ProfileInfoContainer: {
+        flex: 70,
+        // backgroundColor: 'blue',
+        flexDirection: 'row',
+        width: '100%'
+    },
+
+    ProfileImageContainer: {
+        flex: 40,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+        // backgroundColor: 'blue'
+    },
+
+    ProfileNameContainer: {
+        flex: 60,
+        backgroundColor: 'yellow'
+    },
+
+    ProfileImage: {
+        height: 110,
+        width: 110,
+        borderRadius : 55,
+        borderWidth : 2,
+        borderColor : '#014421'
+    },
+
+    ProfileToggleContainer: {
+        flex: 30,
+        // backgroundColor: 'yellow',
+        flexDirection: 'row',
+        width: '100%'
+    },
+
+    MedalsContainer: {
+        flex: 55,
+        flexDirection: 'column',
+        // backgroundColor: 'yellow'
     }
 });
 
--- pages/profilePage.tsx
+++ pages/profilePage.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
-  View,
-  Button,
-  StyleSheet
+    View,
+    Button,
+    StyleSheet, Image
 } from 'react-native';
 
 import {ProfileStyles} from "../css/profile/Style";
+import {BACKEND_URL} from '@env';
 import {
 	GoogleSignin,
 } from '@react-native-google-signin/google-signin';
+import axios from "axios/index";
 
 function ProfilePage(props): JSX.Element {
 	// signOut = async () => {
@@ -19,11 +21,63 @@ function ProfilePage(props): JSX.Element {
 	// 	}
 	// };
 
-  return (
-	  <View style={ProfileStyles.Background}>
+    const [profilePhotoURL, setProfilePhotoURL] = useState("")
 
-	  </View>
-  )
+    useEffect(() => {
+        getProfilePhoto()
+    }, [])
+    const getProfilePhoto = function() {
+        var config = {
+            method: 'post',
+            url: BACKEND_URL + 'sign_up/get_profile_photo',
+            withCredentials: true,
+            credentials: 'include',
+            headers: {
+                Accept: 'application/json',
+            }
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(response.data)
+                setProfilePhotoURL(response.data)
+            })
+            .catch((error) =>
+                console.log(error)
+            )
+    }
+
+    return (
+      <View style={ProfileStyles.Background}>
+          <View style={ProfileStyles.TopSeparator}>
+
+          </View>
+          <View style={ProfileStyles.ProfileContainer}>
+              <View style={ProfileStyles.ProfileCard}>
+                <View style={ProfileStyles.ProfileInfoContainer}>
+                    <View style={ProfileStyles.ProfileImageContainer}>
+                        <Image
+                            src={profilePhotoURL}
+                            style={ProfileStyles.ProfileImage}
+                        >
+                        </Image>
+                    </View>
+
+                    <View style={ProfileStyles.ProfileNameContainer}>
+
+                    </View>
+                </View>
+                <View style={ProfileStyles.ProfileToggleContainer}>
+
+                </View>
+              </View>
+          </View>
+          <View style={ProfileStyles.MedalsContainer}>
+
+          </View>
+
+      </View>
+    )
 }
 
 export default ProfilePage;
