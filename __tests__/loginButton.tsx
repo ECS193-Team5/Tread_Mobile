daaf5f71ddf27dae61cee84eead7c5902d08d352
pdkainth <33015564+pdkainth@users.noreply.github.com>
2023-05-22 14:38:09 -0700

    saved config for google sign in

--- __tests__/loginButton.tsx
+++ __tests__/loginButton.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import LoginButton from "../components/login/loginButton";
-
-test('login button renders correctly')
+test('login button renders correctly', () => {
+  // jest.mock('@react-native-google-signin/google-signin', () => {});
+  const tree  = renderer.create(<LoginButton filled = {true} text = {'Testing'}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+})
