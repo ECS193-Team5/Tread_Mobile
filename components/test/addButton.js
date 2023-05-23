ca49002659ee3c8ee781e39ce0b782fbaeb58f3b
pdkainth <33015564+pdkainth@users.noreply.github.com>
2023-05-23 11:12:06 -0700

    Added add button javascript

---
+++ components/test/addButton.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+	Text,
+	View,
+	Image,
+  TouchableHighlight
+} from 'react-native';
+
+import {styles} from '../../css/add/home/Style';
+import LinearGradient from 'react-native-linear-gradient';
+
+function AddButton({onPress, title, description, imageUrl}) {
+	return (
+			<TouchableHighlight onPress = {onPress} style = {styles.Button}>
+				<LinearGradient
+          colors={['rgba(1, 63, 33,1)', 'rgba(1, 63, 33,0.5)', 'rgb(1, 100, 0)']}
+          style={styles.GradientStyle}
+					>
+					<View style = {styles.IconContainer}>
+					  <Image
+					    src = {imageUrl}
+              style = {styles.Image}
+					    >
+					  </Image>
+					</View>
+
+					<View style = {styles.TextContainer}>
+						<View style = {styles.TitleContainer}>
+							<Text style = {styles.Title}>{title}</Text>
+						</View>
+
+						<View style = {styles.DescContainer}>
+							<Text style = {styles.Description}>{description}</Text>
+						</View>
+
+					</View>
+
+				</LinearGradient>
+			</TouchableHighlight>
+	)
+}
+
+export default AddButton;
