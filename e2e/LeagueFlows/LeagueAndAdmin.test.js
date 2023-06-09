370f784c09e40aea7c90c1a72e977fbb51c98503
Kaushik119 <32000979+Kaushik119@users.noreply.github.com>
2023-06-08 21:27:33 -0700

    added league flows

---
+++ e2e/LeagueFlows/LeagueAndAdmin.test.js
@@ -0,0 +1,19 @@
+describe('Check for Leagues and Admin Leagues', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  it('Switch to Leagues Tab', async () => {
+    await element(by.label('Leagues Nav')).tap()
+    await expect(element(by.text('Your Leagues'))).toBeVisible();
+  });
+
+  it('Check for Leagues', async () => {
+    await expect(element(by.text('justice!!!'))).toBeVisible();
+  });
+
+  it('Tap on Admin Tab and Check admin League', async () => {
+    await (element(by.text('Admin'))).tap();
+    await expect(element(by.text('Test League'))).toBeVisible();
+  });
+});
